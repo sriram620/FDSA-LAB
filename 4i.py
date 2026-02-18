76e622eb6d812ee9026eae0428674ea39e805cba
sriram620 <exipereditt@gmail.com>
2026-02-18 13:40:02 +0530

    Add variance calculations for multiple samples

---
+++ 4i.py
@@ -0,0 +1,12 @@
+from statistics import variance from 
+fractions import Fraction as fr 
+sample1 = (1, 2, 5, 4, 8, 9, 12) 
+sample2 = (-2, -4, -3, -1, -5, -6) 
+sample3 = (-9, -1, -0, 2, 1, 3, 4, 19) 
+sample4 = (fr(1, 2), fr(2, 3), fr(3, 4),fr(5, 6), fr(7, 8)) 
+sample5 = (1.23, 1.45, 2.1, 2.2, 1.9) 
+print("Variance of Sample1 is ",variance(sample1)) 
+print("Variance of Sample2 is ",variance(sample2)) 
+print("Variance of Sample3 is ",variance(sample3)) 
+print("Variance of Sample4 is ", variance(sample4)) 
+print("Variance of Sample5 is ",variance(sample5))
