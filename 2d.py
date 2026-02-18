9bc384ac7136bbf8d3fd21ec7ea77adee7cd719c
sriram620 <exipereditt@gmail.com>
2026-02-18 13:30:58 +0530

    Create DataFrame and check for null values
    Added a script to create a DataFrame from a dictionary and check for null values.

---
+++ 2d.py
@@ -0,0 +1,13 @@
+# importing pandas as pd 
+import pandas as pd 
+# importing numpy as np 
+importnumpy as np 
+# dictionary of lists 
+dict = {'First Score':[100, 90, np.nan, 95], 
+'Second Score': [30, 45, 56, np.nan], 
+'Third Score':[np.nan, 40, 80, 98]} 
+ 
+# creating a dataframe from list df = 
+pd.DataFrame(dict) 
+# using isnull() function 
+df.isnull()
