241555b8a3e538d10069d50660a4ff179549d89b
sriram620 <exipereditt@gmail.com>
2026-02-18 13:34:31 +0530

    Implement basic plotting examples in 3d.py
    Added examples for line plot, bar plot, histogram, and scatter plot using matplotlib.

---
+++ 3d.py
@@ -0,0 +1,26 @@
+Line plot : 
+ 
+from matplotlib import pyplot as plt x 
+= [5, 2, 9, 4, 7] 
+y = [10, 5, 8, 4, 2] 
+ 
+plt.plot(x,y) plt.show() 
+ 
+Bar plot : 
+from matplotlib import pyplot as plt x 
+= [5, 2, 9, 4, 7] 
+y = [10, 5, 8, 4, 2] 
+ 
+plt.bar(x,y) plt.show() 
+Histogram : 
+ 
+from matplotlib import pyplot as plt y 
+y= [10, 5, 8, 4, 2] 
+plt.hist(y) plt.show() 
+Scatter Plot : 
+ 
+from matplotlib import pyplot as plt x 
+= [5, 2, 9, 4, 7] 
+y = [10, 5, 8, 4, 2] 
+plt.scatter(x, y)  
+plt.show()
